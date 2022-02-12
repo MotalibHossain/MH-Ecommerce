a71e734640cff553e5b185e4da5d1e9880bb4125
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-02-12 14:21:24 +0600

    Complete the template rendering

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,21 @@
 from unicodedata import name
 from django.urls import path, include
-from shop.views import Home, About
+from shop.views import (
+    Home,
+    Shop,
+    Features,
+    Blog,
+    Contact, 
+    About,
+)
 
 app_name="shop"
 
 urlpatterns = [
     path('', Home, name="Home"),
+    path('shop/', Shop, name="Shop"),
+    path('features/', Features, name="Features"),
+    path('blog/', Blog, name="Blog"),
+    path('contact/', Contact, name="Contact"),
     path('about/', About, name="About"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -6,5 +6,21 @@ def Home(request):
     return render(request, 'shop/index.html')
 
     
+def Shop(request):
+    return render(request, 'shop/product.html')
+
+    
+def Features(request):
+    return render(request, 'shop/shoping-cart.html')
+
+
+def Blog(request):
+    return render(request, 'shop/blog.html')
+
+
+def Contact(request):
+    return render(request, 'shop/contact.html')
+
+
 def About(request):
     return render(request, 'shop/about.html')
