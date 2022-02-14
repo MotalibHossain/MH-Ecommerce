0a846db709d0819648ba701311e9432bdcd4e7c0
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-02-14 23:17:17 +0600

    register created models

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
+import django
 from django.contrib import admin
+from shop.models import Catagory, Product
 
 # Register your models here.
+@admin(Catagory)
+class CatagoryAdmin(admin.ModelAdmin):
+    list_display=['name', 'slug']
+    prepopulated_filds={'slug':('name')}
+
+
+@admin(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['name', 'catagory', 'price', 'is_stock', 'publish_date']
+    list_filter=['is_stock', 'is_active']
+    list_editable=['price', 'is_active', 'is_stock']
