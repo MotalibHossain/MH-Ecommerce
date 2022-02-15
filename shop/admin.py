4c06f45a56ce3b683938b457e25c60b2ae4f9bb9
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-02-15 23:54:57 +0600

    Update footer

--- shop/admin.py
+++ shop/admin.py
@@ -3,14 +3,14 @@ from django.contrib import admin
 from shop.models import Catagory, Product
 
 # Register your models here.
-@admin(Catagory)
+@admin.register(Catagory)
 class CatagoryAdmin(admin.ModelAdmin):
-    list_display=['name', 'slug']
+    list_display=['name']
     prepopulated_filds={'slug':('name')}
 
 
-@admin(Product)
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display=['name', 'catagory', 'price', 'is_stock', 'publish_date']
-    list_filter=['is_stock', 'is_active']
-    list_editable=['price', 'is_active', 'is_stock']
+    list_display=['name', 'catagory', 'price', 'is_stock', 'published_date']
+    list_filter=['is_active', 'is_stock' ]
+    list_editable=['price', 'is_stock', 'is_stock']
