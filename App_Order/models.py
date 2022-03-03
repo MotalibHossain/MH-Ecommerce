a145c33b71c214bdaa0d384cc6701f1bc64bea58
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-03-03 23:28:59 +0600

    add Order model in App_Order app

--- App_Order/models.py
+++ App_Order/models.py
@@ -1,9 +1,12 @@
+from pyexpat import model
 from turtle import update
 from urllib import request
+from venv import create
 from django.shortcuts import redirect
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
+from App_Order.views import card
 from shop.models import Product
 
 # Create your models here.
@@ -24,3 +27,21 @@ class Card(models.Model):
         return total
     def get_absolute_url(self):
         return reverse_lazy('App_Order:card', args=[self.id])
+
+class Order(models.Model):
+    orderItem=models.ManyToManyField(Card)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    ordered=models.BooleanField(default=False)
+    orderId=models.CharField(max_length=80)
+    paymentId=models.CharField(max_length=80)
+    create=models.DateTimeField(auto_now_add=True)
+    update=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user + '===>' + self.orderId
+
+    def orderTotal(self):
+        total=0
+        for orderitem in self.orderItem.objects.all():
+            total+=float(orderitem.totalCost())
+            return total
