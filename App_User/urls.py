71426892a8af52d122b8fc75d4fa90821f317715
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-04-21 22:37:18 +0600

    User Login function convert into calss base view with middleware

--- App_User/urls.py
+++ App_User/urls.py
@@ -4,7 +4,7 @@ from App_User.views import UserLogin, UserLogout
 
 app_name='App_User'
 urlpatterns = [
-    path('user/login', UserLogin , name="UserLogin" ),
+    path('user/login', UserLogin.as_view() , name="UserLogin" ),
     path('user/logout', UserLogout , name="UserLogout" ),
     
 ]
--- App_User/views.py
+++ App_User/views.py
@@ -1,36 +1,68 @@
+from modulefinder import IMPORT_NAME
+from django import views
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.http import HttpResponse
+from django.views import View
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
-def UserLogin(request):
-    if request.method == 'POST':
+# def UserLogin(request):
+#     if request.method == 'POST':
+#         email=request.POST.get('email')
+#         password=request.POST.get('password')
+#         myUrl=myUrl
+#         print("my url is here",myUrl)
+#         user=authenticate(request, username=email, password=password)
+
+#         if user is not None:
+#             login(request, user)
+#             myUrl=request.GET.get('myUrl')
+#             print("my url is here",myUrl)
+
+#             # middleware redirection code 
+
+#             # if myUrl:
+#             #     print("redirect user", myUrl)
+#             #     return HttpResponseRedirect(myUrl)
+#             # else:
+#             #     print("home page",myUrl)
+#             #     # myUrl=None
+#             #     return redirect(reverse_lazy('shop:Home'))
+#         else:
+#             return redirect(reverse_lazy('App_User:UserLogin'))
+
+#     return render(request, 'User/login.html')
+
+
+
+class UserLogin(View):
+    myUrl=None
+
+    def get(self,request):
+        UserLogin.myUrl=request.GET.get('myUrl')
+        print("my url is here",UserLogin.myUrl)
+        return render(request, 'User/login.html')
+
+    def post(self, request):
         email=request.POST.get('email')
         password=request.POST.get('password')
-        myUrl=myUrl
-        print("my url is here",myUrl)
         user=authenticate(request, username=email, password=password)
 
         if user is not None:
             login(request, user)
-            myUrl=request.GET.get('myUrl')
-            print("my url is here",myUrl)
-
             # middleware redirection code 
-            
-            # if myUrl:
-            #     print("redirect user", myUrl)
-            #     return HttpResponseRedirect(myUrl)
-            # else:
-            #     print("home page",myUrl)
-            #     # myUrl=None
-            #     return redirect(reverse_lazy('shop:Home'))
+
+            if UserLogin.myUrl:
+                print("redirect user", UserLogin.myUrl)
+                return HttpResponseRedirect(UserLogin.myUrl)
+            else:
+                print("home page",UserLogin.myUrl)
+                UserLogin.myUrl=None
+            return redirect(reverse_lazy('shop:Home'))
         else:
             return redirect(reverse_lazy('App_User:UserLogin'))
-
-    return render(request, 'User/login.html')
 
 
 def UserLogout(request):
