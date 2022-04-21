460c2feaeff924733d439b39cb890d269414147b
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-04-22 00:31:05 +0600

    Update reteturn url using middleware login

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR/ 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 # ==========================================
         # IMPORTING DOMAIN URL FROM ENV
 # ==========================================
